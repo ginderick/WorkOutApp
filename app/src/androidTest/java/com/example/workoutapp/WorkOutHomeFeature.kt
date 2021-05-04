7e3452f3de3a3991164fbe35aec8240e78ffd072
Gin <57712394+ginderick@users.noreply.github.com>
2021-05-04 21:46:20 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/workoutapp/WorkOutHomeFeature.kt
@@ -0,0 +1,26 @@
+package com.example.workoutapp
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WorkOutHomeFeature: BaseUITest() {
+
+    @Test
+    fun displayStartButton() {
+        onView(withId(R.id.startButton))
+    }
+
+    @Test
+    fun navigateToWorkOutScreen() {
+        onView(withId(R.id.startButton)).perform(click())
+        onView(withId(R.id.workOutDetailsFragment)).check(matches(isDisplayed()))
+    }
+}
